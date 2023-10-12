79393f7df9bb3a33e27dda25654e3a5df413561a
Jaap223 <6003958@mborijnland.nl>
2023-10-12 11:00:06 +0200

    Register
    Registeractivity met styling

--- app/src/main/java/com/example/infomaat2/MainActivity.kt
+++ app/src/main/java/com/example/infomaat2/MainActivity.kt
@@ -10,4 +10,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
+
+
 }
--- app/src/main/java/com/example/infomaat2/RegisterActivity.kt
+++ app/src/main/java/com/example/infomaat2/RegisterActivity.kt
@@ -17,6 +17,7 @@ class RegisterActivity : AppCompatActivity() {
         btnRegister.setOnClickListener {
             insertRegister()
         }
+
     }
 
     private fun insertRegister() {
