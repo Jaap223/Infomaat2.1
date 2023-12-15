0c860ff9c1314f9cc0d72fafebd8a7425f1f83c0
Daan Broek <6018067@mborijnland.nl>
2023-12-15 11:25:22 +0100

    werkt niet maar oke

--- app/src/main/java/com/example/infomaat2/MyDBHelper.kt
+++ app/src/main/java/com/example/infomaat2/MyDBHelper.kt
@@ -1,39 +1,31 @@
 package com.example.infomaat2
-
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
-
 class MyDBHelper (context: Context) : SQLiteOpenHelper(context, "USERDB",null,1) {
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL("CREATE TABLE USERS (USERID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, EMAIL TEXT, PWD TEXT)")
         db?.execSQL("CREATE TABLE OPLEIDINGEN (OPID INTEGER PRIMARY KEY AUTOINCREMENT, naam TEXT, duur TEXT)");
-
+        db?.execSQL("CREATE TABLE POSTS (PID INTEGER PRIMARY KEY AUTOINCREMENT, postnaam TEXT, soort TEXT)");
         getOpleidingen();
         getAllUsers();
-        //updateProfiel();
+//updateProfiel();
     }
-
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-
     }
-
     fun updateProfiel(userId: String, newUserName: String, newPassword: String , newEmail: String) {
         val db = this.writableDatabase
         val values = ContentValues()
         values.put("NAME", newUserName)
         values.put("PWD", newPassword)
         values.put("EMAIL", newEmail)
-
         val rowsAffected = db.update("USERS", values, "USERID = ?", arrayOf(userId))
         db.close()
-
         Log.d("MyDBHelper", "Rows affected: $rowsAffected")
     }
-
     fun loginCheck (mail: String, password: String): Cursor {
         val db = this.readableDatabase
         val selectionArgs = arrayOf(mail, password)
@@ -42,31 +34,26 @@ class MyDBHelper (context: Context) : SQLiteOpenHelper(context, "USERDB",null,1)
             null
         )
     }
-
     fun getAllUsers(): Cursor {
         val db = this.readableDatabase
         return db.rawQuery("SELECT * FROM USERS", null)
     }
-
     fun getOpleidingen(): Cursor {
-        val db =  this.readableDatabase
+        val db = this.readableDatabase
         return db.rawQuery("SELECT * FROM OPLEIDINGEN ", null)
     }
-
     fun deleteUser(userId: String?) {
         val db = this.writableDatabase
         db.delete("USERS", "USERID = ?", arrayOf(userId.toString()))
         db.close()
     }
-
-//    fun updateUser(userId: String?, newUserName: String) {
-//        val db = this.writableDatabase
-//        val values = ContentValues()
-//        values.put("NAME", newUserName)
-//        db.update("USERS", values, "USERID = ?", arrayOf(userId.toString()))
-//        db.close()
-//    }
-
+    // fun updateUser(userId: String?, newUserName: String) {
+// val db = this.writableDatabase
+// val values = ContentValues()
+// values.put("NAME", newUserName)
+// db.update("USERS", values, "USERID = ?", arrayOf(userId.toString()))
+// db.close()
+// }
     fun insertOpleiding(naam: String, duur: String) {
         val db = this.writableDatabase
         val values = ContentValues()
@@ -75,7 +62,6 @@ class MyDBHelper (context: Context) : SQLiteOpenHelper(context, "USERDB",null,1)
         db.insert("OPLEIDINGEN", null, values)
         db.close()
     }
-
     fun updateOpleiding(opId: String, newNaam: String, newDuur: String) {
         val db = this.writableDatabase
         val values = ContentValues()
@@ -85,11 +71,9 @@ class MyDBHelper (context: Context) : SQLiteOpenHelper(context, "USERDB",null,1)
         db.close()
         Log.d("MyDBHelper", "Rows affected in OPLEIDINGEN: $rowsAffected")
     }
-
     fun deleteOpleiding(opId: String?) {
         val db = this.writableDatabase
         db.delete("OPLEIDINGEN", "OPID = ?", arrayOf(opId.toString()))
         db.close()
     }
-
 }
