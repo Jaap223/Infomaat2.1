836d75236693d436f3348b9d01f142f01bf084c7
Daan Broek <6018067@mborijnland.nl>
2023-12-20 11:48:45 +0100

    ?

--- app/src/main/java/com/example/infomaat2/MainActivity.kt
+++ app/src/main/java/com/example/infomaat2/MainActivity.kt
@@ -1,90 +1,91 @@
 package com.example.infomaat2
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.widget.Toolbar // Import Toolbar
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toggle : ActionBarDrawerToggle
+    private lateinit var toggle: ActionBarDrawerToggle
 
-    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
-        val navView : NavigationView = findViewById(R.id.nav_view)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toolbar: Toolbar = findViewById(R.id.toolbar) // Add this line
 
-        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        setSupportActionBar(toolbar) // Add this line
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.nav_home -> Toast.makeText(applicationContext,"Clicked Home",Toast.LENGTH_SHORT).show()
-                R.id.nav_favorite -> Toast.makeText(applicationContext,"Clicked Favorite",Toast.LENGTH_SHORT).show()
-                R.id.nav_postplts -> Toast.makeText(applicationContext,"Clicked Post plaasten",Toast.LENGTH_SHORT).show()
-                R.id.nav_about -> Toast.makeText(applicationContext,"Clicked About",Toast.LENGTH_SHORT).show()
-                R.id.nav_persoverzicht -> Toast.makeText(applicationContext,"Clicked Personen overzicht",Toast.LENGTH_SHORT).show()
-                R.id.nav_inbox -> Toast.makeText(applicationContext,"Clicked Inbox",Toast.LENGTH_SHORT).show()
-                R.id.nav_profile -> Toast.makeText(applicationContext,"Clicked Profile",Toast.LENGTH_SHORT).show()
-                R.id.nav_login -> Toast.makeText(applicationContext,"Clicked Login",Toast.LENGTH_SHORT).show()
-                R.id.nav_share -> Toast.makeText(applicationContext,"Clicked Share",Toast.LENGTH_SHORT).show()
-
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> showToast("Clicked Home")
+                R.id.nav_favorite -> showToast("Clicked Favorite")
+                R.id.nav_postplts -> showToast("Clicked Post plaatsen")
+                R.id.nav_about -> showToast("Clicked About")
+                R.id.nav_persoverzicht -> showToast("Clicked Personen overzicht")
+                R.id.nav_inbox -> showToast("Clicked Inbox")
+                R.id.nav_profile -> showToast("Clicked Profile")
+                R.id.nav_login -> showToast("Clicked Login")
+                R.id.nav_share -> showToast("Clicked Share")
             }
-
             true
         }
 
-
-
         val navigateButton: Button = findViewById(R.id.navigateButton)
         val loginButton: Button = findViewById(R.id.loginButton)
-        val aboutus: Button = findViewById(R.id.btnAboutUs)
+        val aboutUs: Button = findViewById(R.id.btnAboutUs)
 
         navigateButton.setOnClickListener {
             navigateToProfielPagina()
         }
 
-
-        fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-            if (toggle.onOptionsItemSelected(item)){
-                return true
-            }
-
-            return super.onOptionsItemSelected(item)
-
         loginButton.setOnClickListener {
             goToLogin()
+        }
 
+        aboutUs.setOnClickListener {
+            aboutUs()
         }
     }
 
-    fun navigateToProfielPagina() {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun navigateToProfielPagina() {
         val intent = Intent(this, ProfielPaginaActivity::class.java)
         startActivity(intent)
     }
 
-    fun goToLogin() {
+    private fun goToLogin() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
 
-    fun AboutUs () {
+    private fun aboutUs() {
         val intent = Intent(this, AboutUsActivity::class.java)
         startActivity(intent)
     }
 }
-
-
