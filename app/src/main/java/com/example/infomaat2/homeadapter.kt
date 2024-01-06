f3aaea254395c9037d61ec99bbf5d35a38f88bcf
Daan Broek <6018067@mborijnland.nl>
2024-01-06 13:19:35 +0100

    Revert "Revert "haal dit weg stuk""
    This reverts commit 8612697f3cd4300e40fd5cc2a27c254e12fcddb9.

--- app/src/main/java/com/example/infomaat2/homeadapter.kt
+++ app/src/main/java/com/example/infomaat2/homeadapter.kt
@@ -30,40 +30,4 @@ class HomeAdapter(private val postsList: List<Post>) : RecyclerView.Adapter<Home
     override fun getItemCount(): Int {
         return postsList.size
     }
-
-    override fun registerDataSetObserver(p0: DataSetObserver?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun unregisterDataSetObserver(p0: DataSetObserver?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItem(p0: Int): Any {
-        TODO("Not yet implemented")
-    }
-
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("Not yet implemented")
-    }
-
-    override fun getViewTypeCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun isEmpty(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun areAllItemsEnabled(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun isEnabled(p0: Int): Boolean {
-        TODO("Not yet implemented")
-    }
 }
